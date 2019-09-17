b213cd6248eaf029fcc143966ba7a442eed3c750
diwakar <dchoudhary@ftilab.com>
2019-09-17 22:38:21 +0530

    trial

--- initialize_server.py
+++ initialize_server.py
@@ -7,5 +7,5 @@ Handler = http.server.SimpleHTTPRequestHandler
 
 httpd = socketserver.TCPServer(("", PORT), Handler)
 
-print("serving at port", PORT)
+print("serving at port:", PORT)
 httpd.serve_forever()
